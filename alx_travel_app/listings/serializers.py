49e5e2fe527eb91cab2bfb7479d28f35917c6e85
Eve-code93 <evelynejose1993@gmail.com>
2025-07-20 23:19:34 +0300

    add serializers

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,7 +1,29 @@
 from rest_framework import serializers
-from .models import Listing
+from .models import Listing, Booking, Review
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 class ListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Listing
+        fields = '__all__'
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)  # returns username
+    listing = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+    listing = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Review
         fields = '__all__'
